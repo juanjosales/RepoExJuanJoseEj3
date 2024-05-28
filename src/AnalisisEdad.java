5f94699d4d244ec78945b5344a4b80a212276795
juanjosc <juanjo.sales.sc@gmail.com>
2024-05-28 10:13:31 +0200

    subida de la funcionalidad del proyecto

--- src/AnalisisEdad.java
+++ src/AnalisisEdad.java
@@ -1,4 +1,50 @@
+import java.util.ArrayList;
 
-public class AnalisisEdad {
+	public class AnalisisEdad {
+		
+		private ArrayList<Integer> numeros;
 
-}
+		/**
+		 * @param numeros
+		 */
+		public AnalisisEdad(ArrayList<Integer> numeros) {
+			super();
+			this.numeros = numeros;
+		}
+
+		/**
+		 * @return the numeros
+		 */
+		public ArrayList<Integer> getNumeros() {
+			return numeros;
+		}
+
+		/**
+		 * @param numeros the numeros to set
+		 */
+		public void setNumeros(ArrayList<Integer> numeros) {
+			this.numeros = numeros;
+		}
+		
+		public void contarEdades (ArrayList numeros) {
+			int cantmenores = 0, cantmayores = 0, jubilados = 0, edad;
+			
+			for (int i=0;i<numeros.size();i++) {
+				edad = (int) numeros.get(i);
+				if (edad<18) {
+					cantmenores++;
+				}else if (edad>= 18 & edad<56) {
+					cantmayores++;
+				}else {
+					jubilados++;
+				}
+			}
+			
+			System.out.println("La cantidad de menores es :"+cantmenores+" ,la cantidad de mayores es: "+cantmayores+" la cantidad de jubildados es:"+jubilados);
+			
+		}
+
+
+	}
+
+
--- src/PrincipalApp.java
+++ src/PrincipalApp.java
@@ -1,8 +1,27 @@
+import java.util.ArrayList;
 
 public class PrincipalApp {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		ArrayList<Integer>edades = new ArrayList<Integer>();
+		
+		edades.add(54);
+		edades.add(25);
+		edades.add(10);
+		edades.add(889);
+		edades.add(30);
+		edades.add(2);
+		edades.add(4);
+		edades.add(19);
+		edades.add(30);
+
+		
+		
+		AnalisisEdad analisis = new AnalisisEdad (edades);
+		
+		System.out.println(edades);
+		analisis.contarEdades(edades);		
 
 	}
 
